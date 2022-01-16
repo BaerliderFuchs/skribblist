1827313fdfdc72c677d497d7b2c7486e5e03a54d
BaerliderFuchs <marc.baumann2@bluewin.ch>
2022-01-16 01:53:18 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!--     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    </div> -->
+    <header>
+      <h1 class="title is-1">Skribblist</h1>
+      <h1 class="subtitle">
+        Deutsche Wörterlisten für
+        <a href="https://skribbl.io/" target="_blank">Skribbl.io</a>
+      </h1>
+    </header>
+    <router-view class="view" />
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Architects Daughter", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  min-height: 100vh;
+  background: #586b7e;
 }
 
 #nav {
@@ -28,5 +37,44 @@
       color: #42b983;
     }
   }
+}
+
+header {
+  text-align: center;
+  padding-top: 64px;
+
+  .subtitle {
+    color: #363636;
+    font-weight: 600;
+
+    a {
+      color: #42b983;
+    }
+  }
+}
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: transparent;
+  margin: 24px 0;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  border-radius: 100px;
+  background: rgba(255, 255, 255, 0.5);
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgba(255, 255, 255, 0.8);
+}
+
+.view {
+  padding: 64px 15%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,490 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="column">
+      <div class="column-header">
+        <h1 class="title">Bibliothek {{ averageAmountOfWords }}</h1>
+        <div class="search-container">
+          <b-input
+            class="search"
+            v-model="search"
+            placeholder="Suche..."
+          ></b-input>
+          <b-field class="filter" label="Kategorien">
+            <b-select placeholder="Kategorien" v-model="filter">
+              <option
+                v-for="category in categories"
+                :value="category"
+                :key="category"
+              >
+                {{ category }}
+              </option>
+            </b-select>
+          </b-field>
+          <b-field class="filter" label="Sortierung">
+            <b-select
+              placeholder="Sortierung"
+              v-model="sort"
+              @input="sortLibrary()"
+            >
+              <option v-for="order in orders" :value="order" :key="order">
+                {{ order }}
+              </option>
+            </b-select>
+          </b-field>
+        </div>
+      </div>
+      <div class="scroll-container">
+        <!--       <draggable
+        v-model="wordlists"
+        group="wordlists"
+        :sort="false"
+        :emptyInsertThreshold="500"
+        @start="drag = true"
+        @end="drag = false"
+        v-bind="dragOptions"
+      > -->
+        <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+          <section
+            class="card-container"
+            v-for="(wordlist, index) in wordlists"
+            :key="index"
+          >
+            <div
+              v-if="
+                wordlist.name.toLowerCase().includes(search.toLowerCase()) &&
+                (wordlist.categories.includes(filter) || filter == 'Alle')
+              "
+            >
+              <div class="toggle">
+                <b-button
+                  type="is-success"
+                  icon-right="plus"
+                  @click="enableWordlist(index)"
+                />
+              </div>
+              <b-collapse
+                class="card"
+                animation="slide"
+                :open="false"
+                aria-id="contentIdForA11y3"
+              >
+                <template #trigger="props">
+                  <div
+                    class="card-header"
+                    role="button"
+                    aria-controls="contentIdForA11y3"
+                  >
+                    <p class="card-header-title">
+                      {{ wordlist.name
+                      }}<span class="float-right"
+                        >{{ wordlist.words.split(",").length - 1 }} Wörter</span
+                      >
+                    </p>
+                    <a class="card-header-icon">
+                      <b-icon :icon="props.open ? 'menu-down' : 'menu-up'">
+                      </b-icon>
+                    </a>
+                  </div>
+                </template>
+
+                <div class="card-content">
+                  <div class="content">
+                    {{ wordlist.words }}
+                  </div>
+                </div>
+              </b-collapse>
+            </div>
+          </section>
+        </transition-group>
+        <!--       </draggable>
+ -->
+      </div>
+    </div>
+    <div class="column">
+      <div class="column-header">
+        <h1 class="title">
+          Ausgewählt ({{ enabledWordlists.length }}/{{ totalAmountOfLists }})
+        </h1>
+        <b-button
+          class="remove-all-button"
+          type="is-danger"
+          @click="disableAllLists()"
+          :disabled="totalWordCount < 1"
+          icon-left="delete"
+        >
+          Alle entfernen
+        </b-button>
+        <b-tooltip
+          label="Entfernt zufällige Wörter von grossen Listen, damit deren Wörter nicht zu häufig im Vergleich zu den kleineren Listen auftreten."
+          type="is-light"
+          multilined
+        >
+          <b-field class="balance-words">
+            <b-switch
+              v-model="balanceWords"
+              type="is-warning"
+              @input="getTotalWordCount()"
+              >Listen ausbalancieren</b-switch
+            >
+          </b-field>
+        </b-tooltip>
+
+        <b-button
+          class="copy-button"
+          type="is-success"
+          @click="copyAll()"
+          :disabled="totalWordCount < 1"
+          icon-left="content-copy"
+        >
+          {{ totalWordCount }} Wörter kopieren
+        </b-button>
+      </div>
+      <div class="scroll-container">
+        <!--       <draggable
+        class="draggable-container"
+        v-model="enabledWordlists"
+        group="wordlists"
+        :sort="false"
+        :emptyInsertThreshold="500"
+        @start="drag = true"
+        @end="drag = false"
+        v-bind="dragOptions"
+      > -->
+        <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+          <section
+            class="card-container"
+            v-for="(wordlist, index) in enabledWordlists"
+            :key="index"
+          >
+            <div>
+              <div class="toggle">
+                <b-button
+                  type="is-danger"
+                  icon-right="minus"
+                  @click="disableWordlist(index)"
+                />
+              </div>
+              <b-collapse
+                class="card"
+                animation="slide"
+                :open="false"
+                aria-id="contentIdForA11y3"
+              >
+                <template #trigger="props">
+                  <div
+                    class="card-header"
+                    role="button"
+                    aria-controls="contentIdForA11y3"
+                  >
+                    <p class="card-header-title">
+                      {{ wordlist.name
+                      }}<span class="float-right"
+                        >{{ wordlist.words.split(",").length - 1 }} Wörter</span
+                      >
+                    </p>
+                    <a class="card-header-icon">
+                      <b-icon :icon="props.open ? 'menu-down' : 'menu-up'">
+                      </b-icon>
+                    </a>
+                  </div>
+                </template>
+
+                <div class="card-content">
+                  <div class="content">
+                    {{ wordlist.words }}
+                  </div>
+                </div>
+              </b-collapse>
+            </div>
+          </section>
+        </transition-group>
+        <!--       </draggable>
+ -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import wordlistsJson from "@/wordsets.json";
+/* import draggable from "vuedraggable";
+ */
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    /* draggable */
+  },
+  data() {
+    return {
+      wordlists: wordlistsJson,
+      totalAmountOfLists: 0,
+      averageAmountOfWords: 0,
+      enabledWordlists: [],
+      categories: [],
+      totalWordCount: 0,
+      search: "",
+      drag: false,
+      filter: "Alle",
+      balanceWords: false,
+      orders: [
+        "Aplhabetisch ↓",
+        "Aplhabetisch ↑",
+        "Anzahl Wörter ↓",
+        "Anzahl Wörter ↑",
+      ],
+      sort: "Aplhabetisch ↓",
+    };
+  },
+  created() {
+    this.sortLibrary();
+    this.categories.push("Alle");
+    this.wordlists.forEach((wordlist) => {
+      this.averageAmountOfWords += wordlist.words.split(",").length - 1;
+      wordlist.categories.forEach((category) => {
+        if (!this.categories.includes(category)) this.categories.push(category);
+      });
+    });
+    this.totalAmountOfLists = this.wordlists.length;
+    this.averageAmountOfWords /= this.totalAmountOfLists;
+
+    this.categories.sort(function (a, b) {
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    });
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "wordlists",
+        disabled: false,
+        ghostClass: "ghost",
+        sort: false,
+      };
+    },
+  },
+  methods: {
+    async copyAll() {
+      let finalWordlist = "";
+      if (this.balanceWords) {
+        finalWordlist = this.balanceWordlists();
+      } else {
+        this.enabledWordlists.forEach((wordlist) => {
+          finalWordlist = finalWordlist.concat(wordlist.words);
+        });
+      }
+
+      try {
+        await navigator.clipboard.writeText(finalWordlist);
+        this.$buefy.notification.open({
+          message: `${this.totalWordCount} Wörter in die Zwischenablage kopiert`,
+          type: "is-success",
+          hasIcon: true,
+        });
+      } catch ($e) {
+        this.$buefy.notification.open({
+          message: "Etwas ist schief gelaufen",
+          type: "is-danger",
+          hasIcon: true,
+        });
+      }
+    },
+    disableAllLists() {
+      this.disableWordlist(0);
+      if (this.enabledWordlists.length) {
+        this.disableAllLists();
+      }
+    },
+    getTotalWordCount() {
+      let wordCount = 0;
+      if (this.balanceWords) {
+        this.totalWordCount = this.balanceWordlists().split(",").length - 1;
+      } else {
+        this.enabledWordlists.forEach((wordlist) => {
+          wordCount += wordlist.words.split(",").length - 1;
+        });
+        this.totalWordCount = wordCount;
+      }
+    },
+    disableWordlist(i) {
+      this.wordlists.push(this.enabledWordlists[i]);
+      this.enabledWordlists = this.enabledWordlists.filter(
+        (wordlist) => wordlist !== this.enabledWordlists[i]
+      );
+      this.getTotalWordCount();
+      this.sortLibrary();
+    },
+    enableWordlist(i) {
+      this.enabledWordlists.push(this.wordlists[i]);
+      this.wordlists = this.wordlists.filter(
+        (wordlist) => wordlist !== this.wordlists[i]
+      );
+      this.getTotalWordCount();
+    },
+    sortLibrary() {
+      switch (this.sort) {
+        case this.orders[1]:
+          this.wordlists.sort(function (a, b) {
+            if (a.name < b.name) {
+              return 1;
+            }
+            if (a.name > b.name) {
+              return -1;
+            }
+            return 0;
+          });
+          break;
+        case this.orders[2]:
+          this.wordlists.sort(function (a, b) {
+            return b.words.split(",").length - a.words.split(",").length;
+          });
+          break;
+        case this.orders[3]:
+          this.wordlists.sort(function (a, b) {
+            return a.words.split(",").length - b.words.split(",").length;
+          });
+          break;
+        default:
+          this.wordlists.sort(function (a, b) {
+            if (a.name < b.name) {
+              return -1;
+            }
+            if (a.name > b.name) {
+              return 1;
+            }
+            return 0;
+          });
+          break;
+      }
+    },
+    balanceWordlists() {
+      let min = 100000;
+      let max = 0;
+      let avg = 0;
+
+      let finalWordString = "";
+
+      this.enabledWordlists.forEach((wordlist) => {
+        let length = wordlist.words.split(",").length + 1;
+        if (length > max) max = length;
+        if (length < min) min = length;
+        avg += length;
+      });
+      avg = Math.round(avg / this.enabledWordlists.length);
+
+      this.enabledWordlists.forEach((wordlist) => {
+        if (wordlist.words.split(",").length + 1 < avg) {
+          finalWordString += wordlist.words;
+          return;
+        }
+        let wordArray = wordlist.words.split(",");
+        wordArray.pop();
+        while (wordArray.length >= avg - 1) {
+          wordArray.splice(Math.floor(Math.random() * wordArray.length), 1);
+        }
+        let newWordlistString = "";
+        wordArray.forEach((word) => {
+          newWordlistString += word + ", ";
+        });
+        finalWordString += newWordlistString;
+      });
+
+      return finalWordString;
+    },
   },
 };
 </script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  flex-direction: row;
+}
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.ghost {
+  opacity: 0.5;
+}
+
+.column {
+  width: 50%;
+
+  &:first-of-type {
+    border-right: 2px dashed black;
+  }
+
+  .column-header {
+    position: relative;
+    background: #586b7e;
+    padding-bottom: 12px;
+    //box-shadow: 0px 10px 20px 15px #586b7e;
+    z-index: 1000;
+    border-bottom: 2px dashed black;
+    height: 114px;
+
+    .search-container {
+      display: flex;
+      flex-direction: row;
+
+      .search {
+        flex: auto;
+      }
+      .filter {
+        margin-top: -32px;
+        margin-left: 24px;
+      }
+    }
+  }
+
+  .scroll-container {
+    height: 67vh;
+    overflow-y: scroll;
+    padding: 0 8px;
+  }
+}
+.draggable-container {
+  height: 200px;
+}
+.toggle {
+  position: absolute;
+  right: 4px;
+  top: 4px;
+  z-index: 100;
+}
+.remove-all-button {
+  float: left;
+}
+.balance-words {
+  float: left;
+  margin: 8px 24px;
+  color: #363636;
+  font-weight: 600;
+}
+.copy-button {
+  float: right;
+}
+.card {
+  margin: 24px 0;
+  transition: 0.2s;
+}
+.disabled {
+  opacity: 0.6;
+}
+.float-right {
+  position: absolute;
+  right: 60px;
+  font-weight: normal;
+}
+.card-container {
+  position: relative;
+}
+</style>
